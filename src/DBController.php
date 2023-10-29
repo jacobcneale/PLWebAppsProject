4ec0220966a9379fa63899b5a7794ac879335c7a
jacobcneale <jacobcneale@gmail.com>
2023-10-28 20:35:14 -0400

    Added dummy variables and functions to DBController.php

--- src/DBController.php
+++ src/DBController.php
@@ -1,1 +1,47 @@
+<?php
 
+class DBController {
+
+    private $psswd;
+    private $usrnm;
+
+    private $postTable;
+    private $userTable;
+    private $restaurantTable;
+    private $eventTable;
+
+    public function __construct($input){
+        return null;
+    }
+
+    public function connect(){
+        return null;
+    }
+
+    public function disconnect(){
+        return null;
+    }
+
+    public function createTables(){
+        return null;
+    }
+
+    public function addItem($item, $table){
+        return null;
+    }
+
+    public function delItem($item, $table){
+        return null;
+    }
+
+    public function getItem($item, $table){
+        return null;
+    }
+
+    public function editItem($item,$table,$column,$data){
+        return null;
+    }
+
+
+
+}
