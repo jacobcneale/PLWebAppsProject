94ed44805da57cb31eeb78b25796a0116cc3fa27
Eric Li <lieric021@gmail.com>
2023-11-01 17:15:28 -0400

    added database files

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 spl_autoload_register(function ($classname) {
-    include "/opt/src/trivia/$classname.php";
+    include "$classname.php";
 });
         
 
