92b084407c7e3b69849739d4bebb92704c50caab
jacobcneale <jacobcneale@gmail.com>
2023-11-02 21:54:40 -0400

    Go home button in login/sign up

--- src/templates/login.php
+++ src/templates/login.php
@@ -8,6 +8,11 @@
   <title>UVAFoodies Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"  crossorigin="anonymous">       
 </head>
+    <div>
+        <form action="?command=welcome" method=post>
+            <button type="submit">Go Home</button>
+      </form>
+    </div>
     <div class="d-flex justify-content-center align-items-center mt-5">
       <div>
         <form action="?command=verify_login" method="post">
